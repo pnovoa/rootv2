f94fdae5b82c5547faabbe115f4c29361e979849
pavel <pavel@titanAE>
2018-09-28 11:42:12 -0500

    Prototyping v1

---
+++ src/main/java/rootv2/rootexp/RMPBTestProblem.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package rootv2.rootexp;
+
+import java.util.Vector;
+
+import rootv2.problems.ObjectiveFunction;
+import rootv2.problems.SearchSpace;
+import rootv2.problems.TestProblem;
+
+/**
+ * @author pavel
+ *
+ */
+public class RMPBTestProblem implements TestProblem<Vector<Double>, Double> {
+
+	@Override
+	public SearchSpace<Vector<Double>> getSearchSpace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ObjectiveFunction<Vector<Double>, Double> getObjectiveFunction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/rootv2/rootexp/RealSearchSpace.java
@@ -0,0 +1,29 @@
+package rootv2.rootexp;
+
+import java.util.Vector;
+
+import rootv2.problems.SearchSpace;
+
+public class RealSearchSpace implements SearchSpace<Vector<Double>> {
+	
+	@Override
+	public Vector<Double> getLowerBound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<Double> getUpperBound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
